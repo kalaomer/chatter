60e0c9fb4235675a3d2a062b2376ee46ef87b7f8
Ömer Kala kalaomer <kalaomer@hotmail.com>
2014-04-10 00:23:31 +0300

    user.py changed

--- user.py
+++ user.py
@@ -1,12 +1,15 @@
 
+import settings
+import socket
 
 class User():
     def __init__(self, client_socket):
         self.client_socket = client_socket
 
+    # This function runs in thread
     def listen(self):
         while True:
             try:
-                message = self.client_socket.recv(SERVER.MAX_PACKAGE_SIZE)
+                message = self.client_socket.recv(settings.SERVER.MAX_PACKAGE_SIZE)
             except socket.error:
                 break
