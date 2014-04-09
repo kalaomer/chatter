df250e183070940903c78363a5cc32ae93468868
Taha Doğan Güneş <tdgunes@gmail.com>
2014-04-10 01:16:59 +0300

    Partially finished user.py

--- user.py
+++ user.py
@@ -1,22 +1,54 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import socket
+import threading
 
 import settings
 
 
-class User():
-    def __init__(self, user_socket):
+class UserThread(threading.Thread):
+    def __init__(self, user_socket, users):
+        threading.Thread.__init__(self)
         self.user_socket = user_socket
+        self.users = users
+        self.name = None
 
+    def run(self):
+        connected = True
 
-    def listen(self):
-        """
-        This function runs in a thread
-        """
-        while True:
-            try:
-                message = self.user_socket.recv(settings.SERVER.MAX_PACKAGE_SIZE)
-            except socket.error:
-                break
+        text = self.receive()
+        while connected:
+            if text in ["quit", "bye"]:
+                self.send('you shall never be forgotten!')
+                connected = False
+            elif self.is_name_command(text):
+                self.name = text.split(" ")[1]
+                self.send("you are now known as '%s'" % self.name)
+            else:
+                name = "" if not self.name else self.name
+                self.send_all("%s: %s" % (name, text))
+
+            text = self.receive()
+
+        self.user_socket.close()
+
+    def send(self, text):
+        self.user_socket.send(text.strip() + "\n")
+
+    def send_all(self, text):
+        for name in self.users.keys():
+            if name != self.getName():
+                self.users[name].send(text.strip() + "\n")
+
+    def receive(self):
+        result = self.client.recv(settings.SERVER.MAX_PACKAGE_SIZE)
+        if result:
+            result = result.strip()
+        return result
+
+    @staticmethod
+    def is_name_command(text):
+        if text > 5 and text[:5] == "name ":
+            return len(text.split(" ")[1]) > 0
+        else:
+            return False
